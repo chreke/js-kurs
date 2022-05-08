ff1cb734409c8b9738318f2984442f6d530fefe2
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-05-08 18:30:28 +0200

    Refactor tests to use common base URL

--- playwright-intro/tests/objektvision.test.js
+++ playwright-intro/tests/objektvision.test.js
@@ -1,10 +1,11 @@
 const { test, expect } = require("@playwright/test");
 
-// test.use({ launchOptions: { slowMo: 50 } });
+
+const BASE_URL = "https://objektvision.se";
 
 test.describe("Search", () => {
   test("Search without input", async ({ page }) => {
-    await page.goto("https://objektvision.se/");
+    await page.goto(BASE_URL);
     await page
       .locator("#search-buttoncontainer__top .btn-estate-search")
       .click();
@@ -12,7 +13,7 @@ test.describe("Search", () => {
   });
 
   test("Search for 'Bromma'", async ({ page }) => {
-    await page.goto("https://objektvision.se/");
+    await page.goto(BASE_URL);
     await page.locator("#search__geo-input").type("Bromma");
     await page.locator("li[data-areatype]").first().click();
     await page
@@ -24,7 +25,7 @@ test.describe("Search", () => {
 
 test.describe("Logged in", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://objektvision.se/");
+    await page.goto(BASE_URL);
     await page.locator(".login-link.nav-link").first().click();
     await page.locator("#Username").fill("ekeroth.christoffer@gmail.com");
     await page.locator("#Password").fill("supersecret123");
@@ -32,14 +33,14 @@ test.describe("Logged in", () => {
   });
 
   test("Watch for search results", async ({ page }) => {
-    await page.goto("https://objektvision.se/lediga_lokaler/stockholm/bromma");
+    await page.goto(`${BASE_URL}/lediga_lokaler/stockholm/bromma`);
     await page.locator("text=Bevaka sökning").click();
     await page.locator("text=Spara sökning").click();
     await expect(page.locator("text=Bevakad sökning")).toBeVisible();
   });
 
   test.afterEach(async ({ page }) => {
-    await page.goto("https://objektvision.se/bevakningar");
+    await page.goto(`${BASE_URL}/bevakningar`);
     let watchCount = (await page.$$(".icon-trash")).length;
     while (watchCount) {
       await page.locator(".icon-trash").click();
